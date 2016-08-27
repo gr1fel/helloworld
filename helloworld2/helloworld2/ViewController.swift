afb9c2511ee9672705f2a592ba72dcb8933b0498
gr1fel <my@13px.ru>
2016-08-27 10:24:14 +0300

    initial commit

---
+++ helloworld2/helloworld2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloworld2
+//
+//  Created by Alexey Grigorev on 8/27/16.
+//  Copyright © 2016 Alexey Grigorev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
